a94e4b8769bbcbfd5a658757477660a2ba60d9f5
DASV1985 <diegoalonsosanchezvasquez@gmail.com>
2025-03-24 01:42:04 -0400

    new commit

---
+++ src/Partie_5.cpp
@@ -0,0 +1,16 @@
+
+
+# include <Arduino.h>
+
+int potln;
+int potPin = 2; // Selection de la broche d'entrée du potentiomètre
+
+void setup() {
+    Serial.begin(9600);
+}
+
+void loop() {
+    
+}
+
+// damn you
